4850cda0e5a4a5c94f5c0522f51d5f77930c5588
Alex Harvey <alexharv074@gmail.com>
2020-01-01 22:25:11 +1100

    Add to add_2_more.sh

--- add_2_more.sh
+++ add_2_more.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+vpc_id='xxx'
+
 for i in 3 4 ; do
   group_name="test-group-$i"
-  aws ec2 create-security-group --description "Test group $i" \
-    --group-name "$group_name"
+  aws ec2 create-security-group \
+    --description "Test group $i" \
+    --group-name "$group_name" \
+    --vpc-id "$vpc_id"
   aws ec2 authorize-security-group-ingress \
     --group-name "$group_name" \
     --protocol 'tcp' \
